e9054f134c236fc3727b424af9c7bb87ba4ef7b1
Javier Fornells Calvo <fornelc@gmail.com>
2017-10-26 14:24:56 +0200

    Refactoring BusyFlightsConverterTest.java

--- src/test/java/com/travix/medusa/busyflights/mock/BusyFlightsMock.java
+++ src/test/java/com/travix/medusa/busyflights/mock/BusyFlightsMock.java
@@ -1,4 +1,87 @@
 package com.travix.medusa.busyflights.mock;
 
+import com.travix.medusa.busyflights.domain.busyflights.BusyFlightsRequest;
+import com.travix.medusa.busyflights.domain.crazyair.CrazyAirRequest;
+import com.travix.medusa.busyflights.domain.crazyair.CrazyAirResponse;
+import com.travix.medusa.busyflights.domain.toughjet.ToughJetRequest;
+import com.travix.medusa.busyflights.domain.toughjet.ToughJetResponse;
+import com.travix.medusa.busyflights.repository.impl.CrazyAirRepositoryImpl;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class BusyFlightsMock {
+
+    static DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    static String date1 = "20/10/2017";
+    static String date2 = "26/10/2017";
+
+    static DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+    static String dateTime1 = "2017-10-20 00:00";
+    static String dateTime2 = "2017-10-26 00:00";
+
+    static String dateTime3 = "2017-10-20T00:00:00Z";
+    static String dateTime4 = "2017-10-26T00:00:00Z";
+
+    public static CrazyAirRequest getCrazyAirRequestMock(){
+        CrazyAirRequest crazyAirRequest = new CrazyAirRequest();
+        crazyAirRequest.setOrigin("TOU");
+        crazyAirRequest.setDestination("UOT");
+        crazyAirRequest.setReturnDate(LocalDate.parse(date2, formatter1));
+        crazyAirRequest.setDepartureDate(LocalDate.parse(date1, formatter1));
+        crazyAirRequest.setPassengerCount(100);
+
+        return crazyAirRequest;
+    }
+
+    public static BusyFlightsRequest getBusyFlightsRequestMock(){
+        BusyFlightsRequest busyFlightsRequest = new BusyFlightsRequest();
+        busyFlightsRequest.setOrigin("TOU");
+        busyFlightsRequest.setDestination("UOT");
+        busyFlightsRequest.setReturnDate(LocalDate.parse(date2, formatter1));
+        busyFlightsRequest.setDepartureDate(LocalDate.parse(date1, formatter1));
+        busyFlightsRequest.setNumberOfPassengers(100);
+
+        return busyFlightsRequest;
+    }
+
+    public static ToughJetRequest getToughJetRequestMock(){
+        ToughJetRequest toughJetRequest = new ToughJetRequest();
+        toughJetRequest.setFrom("TOU");
+        toughJetRequest.setTo("UOT");
+        toughJetRequest.setInboundDate(LocalDate.parse(date2, formatter1));
+        toughJetRequest.setOutboundDate(LocalDate.parse(date1, formatter1));
+        toughJetRequest.setNumberOfAdults(100);
+
+        return toughJetRequest;
+    }
+
+    public static CrazyAirResponse getCrazyAirResponseMock(){
+        CrazyAirResponse crazyAirResponse = new CrazyAirResponse();
+        crazyAirResponse.setAirline("ABC");
+        crazyAirResponse.setArrivalDate(LocalDateTime.parse(dateTime2, formatter2));
+        crazyAirResponse.setDepartureDate(LocalDateTime.parse(dateTime1, formatter2));
+        crazyAirResponse.setCabinclass("A");
+        crazyAirResponse.setDepartureAirportCode("TOU");
+        crazyAirResponse.setDestinationAirportCode("UOT");
+        crazyAirResponse.setPrice(1000.0d);
+
+        return crazyAirResponse;
+    }
+
+    public static ToughJetResponse getToughJetResponseMock(){
+        ToughJetResponse toughJetResponse = new ToughJetResponse();
+        toughJetResponse.setDepartureAirportName("TOU");
+        toughJetResponse.setArrivalAirportName("UOT");
+        toughJetResponse.setCarrier("ABC");
+        toughJetResponse.setBasePrice(1000.0d);
+        toughJetResponse.setDiscount(10);
+        toughJetResponse.setTax(10);
+        toughJetResponse.setInboundDateTime(Instant.parse(dateTime3));
+        toughJetResponse.setOutboundDateTime(Instant.parse(dateTime4));
+
+        return toughJetResponse;
+    }
 }
--- src/test/java/com/travix/medusa/busyflights/repository/converter/BusyFlightsConverterTest.java
+++ src/test/java/com/travix/medusa/busyflights/repository/converter/BusyFlightsConverterTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import com.travix.medusa.busyflights.mock.BusyFlightsMock;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +20,6 @@ import com.travix.medusa.busyflights.domain.crazyair.CrazyAirRequest;
 import com.travix.medusa.busyflights.domain.crazyair.CrazyAirResponse;
 import com.travix.medusa.busyflights.domain.toughjet.ToughJetRequest;
 import com.travix.medusa.busyflights.domain.toughjet.ToughJetResponse;
-import com.travix.medusa.busyflights.repository.converter.BusyFlightsConverter;
 
 @RunWith(SpringRunner.class)
 
@@ -43,62 +43,11 @@ public class BusyFlightsConverterTest {
 
     @Before
     public void setUpObjects(){
-
-        DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String date1 = "20/10/2017";
-        String date2 = "26/10/2017";
-
-        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        String dateTime1 = "2017-10-20 00:00";
-        String dateTime2 = "2017-10-26 00:00";
-
-        String dateTime3 = "2017-10-20T00:00:00Z";
-        String dateTime4 = "2017-10-26T00:00:00Z";
-
-        crazyAirRequest = new CrazyAirRequest();
-        crazyAirRequest.setOrigin("TOU");
-        crazyAirRequest.setDestination("UOT");
-        crazyAirRequest.setReturnDate(LocalDate.parse(date2, formatter1));
-        crazyAirRequest.setDepartureDate(LocalDate.parse(date1, formatter1));
-        crazyAirRequest.setPassengerCount(100);
-
-        System.out.println(crazyAirRequest.getDepartureDate());
-
-        busyFlightsRequest = new BusyFlightsRequest();
-        busyFlightsRequest.setOrigin("TOU");
-        busyFlightsRequest.setDestination("UOT");
-        busyFlightsRequest.setReturnDate(LocalDate.parse(date2, formatter1));
-        busyFlightsRequest.setDepartureDate(LocalDate.parse(date1, formatter1));
-        busyFlightsRequest.setNumberOfPassengers(100);
-
-        System.out.println(busyFlightsRequest.getDepartureDate());
-
-        toughJetRequest = new ToughJetRequest();
-        toughJetRequest.setFrom("TOU");
-        toughJetRequest.setTo("UOT");
-        toughJetRequest.setInboundDate(LocalDate.parse(date2, formatter1));
-        toughJetRequest.setOutboundDate(LocalDate.parse(date1, formatter1));
-        toughJetRequest.setNumberOfAdults(100);
-
-        crazyAirResponse = new CrazyAirResponse();
-        crazyAirResponse.setAirline("ABC");
-        crazyAirResponse.setArrivalDate(LocalDateTime.parse(dateTime2, formatter2));
-        crazyAirResponse.setDepartureDate(LocalDateTime.parse(dateTime1, formatter2));
-        crazyAirResponse.setCabinclass("A");
-        crazyAirResponse.setDepartureAirportCode("TOU");
-        crazyAirResponse.setDestinationAirportCode("UOT");
-        crazyAirResponse.setPrice(1000.0d);
-
-        toughJetResponse = new ToughJetResponse();
-        toughJetResponse.setDepartureAirportName("TOU");
-        toughJetResponse.setArrivalAirportName("UOT");
-        toughJetResponse.setCarrier("ABC");
-        toughJetResponse.setBasePrice(1000.0d);
-        toughJetResponse.setDiscount(10);
-        toughJetResponse.setTax(10);
-        toughJetResponse.setInboundDateTime(Instant.parse(dateTime3));
-        toughJetResponse.setOutboundDateTime(Instant.parse(dateTime4));
-
+        crazyAirRequest = BusyFlightsMock.getCrazyAirRequestMock();
+        busyFlightsRequest = BusyFlightsMock.getBusyFlightsRequestMock();
+        toughJetRequest = BusyFlightsMock.getToughJetRequestMock();
+        crazyAirResponse = BusyFlightsMock.getCrazyAirResponseMock();
+        toughJetResponse = BusyFlightsMock.getToughJetResponseMock();
     }
 
     @Test
